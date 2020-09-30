ea5179927409a546d9549e8d6f5e1277c00c52de
Samuel F. Ademola <zion@Ebenezer.local>
2020-09-29 22:25:46 -0500

    finished chapter 1.

--- SwiftUIText/ContentView.swift
+++ SwiftUIText/ContentView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).")
+            .fontWeight(.heavy)
+            .font(.system(size: 20, weight: .bold, design: .rounded))
+            .foregroundColor(.black)
+            .multilineTextAlignment(.center)
+            .lineLimit(nil)
+            .truncationMode(.tail)
+            .lineSpacing(10.0)
+//            .rotationEffect(.degrees(20), anchor: UnitPoint(x: 0, y: 0))
+            .rotation3DEffect(.degrees(60), axis: (x: 1, y: 0, z: 0))
+            .shadow(color: .gray, radius: 2, x: 0, y: 15)
             .padding()
     }
 }
