2ffd00ea76416d60b3fed07918af2163f127b522
Samuel F. Ademola <zion@Ebenezer.local>
2020-11-02 07:33:03 -0600

    init commit.

---
+++ Genesi/GenesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenesiApp.swift
+//  Genesi
+//
+//  Created by Samuel F. Ademola on 11/2/20.
+//
+
+import SwiftUI
+
+@main
+struct GenesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
