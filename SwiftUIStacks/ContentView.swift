85ac44c8fb3a287a8ff95170f58a668fddebd24f
Samuel F. Ademola <zion@Ebenezer.local>
2020-11-04 17:35:26 -0600

    finished excerise 1

--- SwiftUIStacks/ContentView.swift
+++ SwiftUIStacks/ContentView.swift
@@ -25,10 +25,24 @@ struct ContentView: View {
                         .background(Color(red: 255/255, green: 183/255, blue: 37/255))
                         .offset(x: 0, y: 87)
                 }
-                
             }
             .padding(.horizontal)
+            
+            ZStack {
+                PricingView(title: "Team", price: "$299", textColor: .white, bgColor: Color(red: 62/255, green: 63/255, blue: 70/255), icon: "wand.and.rays")
+                    .padding()
+                
+                Text("Perfect for teams with 20 members")
+                    .font(.system(.caption, design: .rounded))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                    .padding(5)
+                    .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                    .offset(x: 0, y: 87)
+            }
+            Spacer()
         }
+        
     }
 }
 
@@ -38,28 +52,42 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+// HEADER-VIEW
 struct HeaderView: View {
     var body: some View {
-        VStack (alignment: .leading, spacing: 2) {
-            
-            Text("Choose")
-                .font(.system(.largeTitle, design: .rounded))
-                .fontWeight(.black)
-            Text("Your Plan")
-                .font(.system(.largeTitle, design: .rounded))
-                .fontWeight(.black)
+        HStack {
+            VStack (alignment: .leading, spacing: 2) {
+                
+                Text("Choose")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+                Text("Your Plan")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+            }
+            Spacer()
         }
+        .padding()
     }
 }
 
+// PRICING-VIEW
 struct PricingView: View {
     var title: String
     var price: String
     var textColor: Color
     var bgColor: Color
+    var icon: String?
     
     var body: some View {
         VStack {
+            
+            icon.map({
+                Image(systemName: $0)
+                    .font(.largeTitle)
+                    .foregroundColor(textColor)
+            })
+            
             Text(title)
                 .font(.system(.title, design: .rounded))
                 .fontWeight(.black)
