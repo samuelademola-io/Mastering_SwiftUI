ebd373c9630a591621536b4320acdaf815cb3e4d
Samuel F. Ademola <zion@Ebenezer.local>
2020-11-12 21:52:49 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mastering_SwiftUI
+ch 09 - Basic Animations and Transitions
---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Samuel F. Ademola on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
