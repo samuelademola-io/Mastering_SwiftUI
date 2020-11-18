f929f5c4d1e266d5182fac9c0244121a5f756418
Samuel F. Ademola <zion@Ebenezer.local>
2020-11-17 23:13:11 -0600

    init commit

---
+++ SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by Samuel F. Ademola on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
