0a6159b3fa833b158275534f5385a285fcefbba4
Samuel Ademola <io.samuelademola@gmail.com>
2020-11-21 18:20:17 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mastering_SwiftUI
+Ch 12 - Playing with Modal Views, Floating Buttons and Alerts
---
+++ SwiftUIModal/SwiftUIModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIModalApp.swift
+//  SwiftUIModal
+//
+//  Created by Samuel F. Ademola on 11/21/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
